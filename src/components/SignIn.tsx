8614ea4fdbf92a301f7e95180697a9c42139a689
Luna Cuevas <68247976+luna-cuevas@users.noreply.github.com>
2023-08-25 10:41:49 -0700

    added missing sign up button to sign in pop up

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -5,6 +5,7 @@ import { supabaseClient } from '@/utils/supabaseClient';
 import { useStateContext } from '@/context/StateContext';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Link from 'next/link';
 
 type Props = {
   setSignInActive: React.Dispatch<React.SetStateAction<boolean>>;
@@ -55,66 +56,79 @@ const SignIn = (props: Props) => {
           props.setSignInActive(false);
         }}
       />
-      <div className="z-[20000001] rounded-2xl fixed max-w-[800px] md:w-2/3 bottom-0 m-auto top-0 flex lg:flex-row flex-col left-0 right-0 h-fit py-8 bg-[#F7F1EE] border-[6px] border-[#7F8019]">
-        <div className="flex lg:w-[40%] ">
-          <h2 className="uppercase font-sans leading-[60px] p-4 lg:p-10 text-right tracking-[0.3rem] m-auto text-2xl lg:text-4xl">
-            Members Login
-          </h2>
-        </div>
-        <div className="flex lg:w-[60%] w-[90%] m-auto ">
-          <ToastContainer
-            position="bottom-right"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="light"
-          />
-          <div className="m-auto w-full">
-            <Auth
-              supabaseClient={supabase}
-              appearance={{
-                style: {
-                  button: {
-                    width: 'fit-content',
-                    maxWidth: '100%',
-                    textTransform: 'uppercase',
-                    background: '#EB8828',
-                    color: 'white',
-                    padding: '0.5rem 2rem',
-                    borderRadius: '0.375rem',
-                    fontFamily: 'sans-serif',
-                  },
-                  input: {
-                    letterSpacing: '0.25rem',
-                    textTransform: 'uppercase',
-                    color: '#F7F1EE',
-                    background: '#7F8019',
-                    borderRadius: '0.375rem',
-                    padding: '1rem 0.75rem',
-                  },
-                },
-              }}
-              localization={{
-                variables: {
-                  sign_in: {
-                    email_label: '',
-                    password_label: '',
-                    email_input_placeholder: 'Email address',
-                    password_input_placeholder: 'Password',
-                    button_label: 'Sign in',
-                  },
-                  sign_up: {
-                    link_text: '',
-                  },
-                },
-              }}
-              providers={[]}
+
+      <div className="z-[20000001] rounded-2xl fixed max-w-[800px] md:w-2/3 bottom-0 m-auto top-0    left-0 right-0 h-fit pt-8 bg-[#F7F1EE] border-[6px] border-[#7F8019] ">
+        <div className="lg:flex-row flex flex-col">
+          <div className="flex lg:w-[40%] ">
+            <h2 className="uppercase font-sans leading-[60px] p-4 lg:p-10 text-right tracking-[0.3rem] m-auto text-2xl lg:text-4xl">
+              Members Login
+            </h2>
+          </div>
+          <div className="flex lg:w-[60%] w-[90%] m-auto ">
+            <ToastContainer
+              position="bottom-right"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="light"
             />
+            <div className="m-auto w-full">
+              <Auth
+                supabaseClient={supabase}
+                appearance={{
+                  style: {
+                    button: {
+                      width: 'fit-content',
+                      maxWidth: '100%',
+                      textTransform: 'uppercase',
+                      background: '#EB8828',
+                      color: 'white',
+                      padding: '0.5rem 2rem',
+                      borderRadius: '0.375rem',
+                      fontFamily: 'sans-serif',
+                    },
+                    input: {
+                      letterSpacing: '0.25rem',
+                      textTransform: 'uppercase',
+                      color: '#F7F1EE',
+                      background: '#7F8019',
+                      borderRadius: '0.375rem',
+                      padding: '1rem 0.75rem',
+                    },
+                  },
+                }}
+                localization={{
+                  variables: {
+                    sign_in: {
+                      email_label: '',
+                      password_label: '',
+                      email_input_placeholder: 'Email address',
+                      password_input_placeholder: 'Password',
+                      button_label: 'Sign in',
+                    },
+                    sign_up: {
+                      link_text: '',
+                    },
+                  },
+                }}
+                providers={[]}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="bg-[#7F8019]  w-fit m-auto px-4 py-1 rounded-t-xl flex">
+          <div className="flex w-fit m-auto gap-2">
+            <p className="text-[#F7F1EE]">Don&apos;t have an account?</p>
+            <Link
+              className="font-sans text-sm text-blue-gray-300"
+              href="/sign-up">
+              Sign up
+            </Link>
           </div>
         </div>
       </div>
