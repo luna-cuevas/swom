4621100235c7413440201d61e35cac1545ea6cb1
Luna Cuevas <68247976+luna-cuevas@users.noreply.github.com>
2024-07-03 12:36:23 -0700

    feat: adding wiki referral page

---
+++ src/app/wiki/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <div className="min-h-[calc(100vh-70px)] flex ">
+      <div className="m-auto">Wiki Page</div>
+    </div>
+  );
+};
+
+export default page;
